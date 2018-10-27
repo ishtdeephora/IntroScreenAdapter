d205e310c0b8e6d45e96904f9096e09bf95594f0
Ishtdeep Hora <ishtdeep.hora@gmail.com>
2018-10-27 08:40:54 +0530

    Commit

---
+++ app/src/main/java/com/example/android/introscreenadapter/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.android.introscreenadapter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pcs on 25-Oct-18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
